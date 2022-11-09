bf8dc8cbcc35f047955c7b59c1d0d17cfeb5084b
dominikmirek <dominik.mirek@hotmail.com>
2022-11-09 12:25:36 +0100

    style

--- zadanie05/index.php
+++ zadanie05/index.php
@@ -6,50 +6,54 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css" ">
 </head>
 <body>
 
 <h1>Dominik Mirek</h1>
-<h2>Zadanie 4</h2>
+<h2>Zadanie 5</h2>
 <p>Napisz program, który oblicza i wyświetla iloraz 2 liczb rzeczywistych z dokładnością do dwóch miejsc po przecinku.</p>
 
 <form method="POST" action="index.php">
-    podaj a :<input type="text" name="a">
-    podaj b :<input type="text" name="b"><br>
+    podaj a :   <input type="text" name="a">
+    podaj b :   <input type="text" name="b"><br><br>
 
     <input type="submit" value="Wyślij">
 </form>
-<?php
+<div class="wynik">
+    <?php
 
 
-if (isset($_POST['a'])) {
-    $a = $_POST['a'];
-    $b = $_POST['b'];
+    if (isset($_POST['a'])) {
+        $a = $_POST['a'];
+        $b = $_POST['b'];
 
-    $s = $a+$b;
-    $r = $a-$b;
-    $iloraz =$a*$b;
+        $s = $a+$b;
+        $r = $a-$b;
+        $iloraz =$a*$b;
 
 
-    echo "a: $a <br>";
-    echo "b: $b <br>";
-    echo "suma: $s <br>";
-    echo "różnica: $r <br>";
-    echo "iloraz: $iloraz <br>";
-    if($b==0){
-        echo "iloczyn: Nie dziel przez 0 <br>";
-        echo "reszta z dzielenia: Nie istnieje <br>";
+        echo "a: $a <br>";
+        echo "b: $b <br>";
+        echo "suma: $s <br>";
+        echo "różnica: $r <br>";
+        echo "iloraz: $iloraz <br>";
+        if($b==0){
+            echo "iloczyn: Nie dziel przez 0 <br>";
+            echo "reszta z dzielenia: Nie istnieje <br>";
+        }
+        else{
+            $iloczyn = floor($a/$b);
+            $m = $a%$b;
+            echo "iloczyn: $iloczyn <br>";
+            echo "reszta z dzielenia: $m <br>";
+        }
+
     }
-    else{
-        $iloczyn = floor($a/$b);
-        $m = $a%$b;
-        echo "iloczyn: $iloczyn <br>";
-        echo "reszta z dzielenia: $m <br>";
-    }
 
-}
+    ?>
+</div>
 
-?>
 </body>
 </html>
 
