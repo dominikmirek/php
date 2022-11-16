ac3b59bee163d46434b94115fefd122589397066
dominikmirek <dominik.mirek@hotmail.com>
2022-11-16 09:14:18 +0100

    tablica

---
+++ tablica_asocjacyjna/index.php
@@ -0,0 +1,11 @@
+<?php
+    $panstwa = array(
+        "Polska"=>"Warszawa",
+        "Niemcy"=>"Berlin",
+        "Francja"=>"Paryz",
+        "Hiszpania"=>"Madryt",
+        "Norwegia"=>"Oslo"
+    );
+
+    print_r($panstwa);
+    echo "<br>Stolicą Polski jest $panstwa[Polska]";
