d4ada1485e3296b666fb1f3ee5023b3d916c498f
dominik <dominik.mirek@hotmail.com>
2023-01-11 12:29:31 +0100

    EGZAMIN

--- zadanie15/index.php
+++ zadanie15/index.php
@@ -5,23 +5,50 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Filmoteka</title>
+    <link rel="stylesheet" href="styl3.css">
 </head>
 <body>
-<?php
-$db = mysqli_connect('localhost','root','','dane2');
-mysqli_set_charset($db,"utf8");
-$q1 = "INSERT INTO rezyserzy VALUES ('','Dominik','Mirek');";
-mysqli_query($db,$q1);
-$q = "SELECT * FROM rezyserzy WHERE 1";
-$wynik = mysqli_query($db,$q);
+<div class="b1">
+    <img src="klaps1.png" alt="Nasze filmy">
+</div>
+<div class="b2">
+    <h1>BAZA FIMLÓW</h1>
+</div>
+<div class="b3">
+    <form method="POST" action="index.php">
+        <section>
+            <option>Sci-FI</option>
+            <option>animacje</option>
+            <option>dramat</option>
+            <option>horror</option>
+            <option>Komedia</option>
+        </section>
+        <input type="button" value="Fimly">
+    </form>
+</div>
+<div class="b4">
+    <img src="gwiezdneWojny.jpg" alt="szturowcy">
+</div>
+<div class="g1">
+    <h2>
+        Wybrano filmy:
+    </h2>
+    <?php
 
-while ($el = mysqli_fetch_row($wynik)){
-    echo $el[0].", ".$el[1].", ".$el[2]."<br>";
-}
+    ?>
+</div>
+<div class="g2">
+    <h2>Wszystkie fimly</h2>
+    <?php
 
-mysqli_close($db);
-?>
+    ?>
+</div>
+<div class="foot">
+    <p>Autor: Dominik Mirek</p>
+    <a href="kwerendy.txt">Zapytania do bazy</a>
+    <a href="fimly.pl" target="_blank">Przejdź do filmy.pl</a>
+</div>
 </body>
 </html>
 
