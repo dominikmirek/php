ba0b0402069478348fd5b1423ca56a038a48b842
3pir02 <dominik.mirek@hotmail.com>
2023-02-10 08:47:04 +0100

    pdo

--- zadanie16/index.php
+++ zadanie16/index.php
@@ -7,6 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<style>
+    table,td{
+        border: solid black 1px;
+    }
+</style>
 <body>
 <br>
 <form  method="POST">
@@ -15,39 +20,36 @@
     <input type="submit" name="wyswietl" value="wyswietl"><br>
     <form>
 <?php
-$db = mysqli_connect('localhost', 'root', '', '3pir_baza_pracownikow');
+$db = mysqli_connect('localhost', 'root', '', '3pir_2_baza_pracownikow');
 mysqli_set_charset($db, 'utf8');
 
 if(isset($_POST['utworz']))
 {
-        $q = "CREATE TABLE prcownicy(id int AUTO_INCREMENT,Nazwisko varchar(50), Imie varchar(50),Stanowisko varchar(50), Dzial varchar(50), Sekcja varchar(50));";
+        $q = "CREATE TABLE IF NOT EXISTS pracownicy(id int,Nazwisko varchar(50), Imie varchar(50),Stanowisko varchar(50), Dzial varchar(50), Sekcja varchar(50));";
         mysqli_query($db, $q);
 }
 if (isset($_POST['zaladuj'])){
-  //  $q = "";
-   // mysqli_query($db, $q);
-    echo biez();
+    $plik = fopen("pracownicy2.txt", "r");
 
+    while(!feof($plik))
+    {
+        $linia = fgets($plik);
+        $slowa = explode(';',$linia);
+        $q = "INSERT INTO pracownicy VALUES('".$slowa[0]."', '".$slowa[1]."', '".$slowa[2]."',' ".$slowa[3]."',' ".$slowa[4]."',' ".$slowa[5]."');";
+        mysqli_query($db, $q);
+    }
 }
  if (isset($_POST['wyswietl'])){
     $q = "SELECT * FROM pracownicy;";
      $wynik = mysqli_query($db, $q);
+     echo "<table>";
      while ($el = mysqli_fetch_row($wynik)) {
-         echo $el[0].", ".$el[1].$el[2].", ".$el[3].", ".$el[4].", ".$el[5].", "."<br>";
+         echo "<tr>";
+         echo "<td>".$el[0]."</td><td> ".$el[1]."</td><td>".$el[2]."</td><td> ".$el[3]."</td><td> ".$el[4]."</td><td>".$el[5]." </td>";
+         echo"</tr>";
      }
+     echo "</table>";
 }
- function biez(){
-     $plik = fopen("pracownicy.csv", "r");
-     $zawartosc = '';
-     while(!feof($plik))
-     {
-         $linia = fgets($plik);
-         $zawartosc .= $linia."<br>";
-     }
-     $zawartosc = mb_convert_encoding($zawartosc, 'UTF-8', "ISO-8859-2");
-      return $zawartosc;
- }
-
 mysqli_close($db);
 ?>
 </body>
